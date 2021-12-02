284f79f6e99bf4ac3ff4ddd700affd444f11a033
Javier Ibanez <expertprep2015@gmail.com>
2021-12-01 20:58:38 -0500

    completed problem 2 putting the interger input into an array

--- leetcode2.js
+++ leetcode2.js
@@ -2,12 +2,19 @@
 
 // An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
 
- 
-
 // Example 1:
 
 // Input: x = 121
 // Output: true
-var isPalindrome = function(x) {
+var isPalindrome = function (x) {
+  let numberArr = x.toString().split("");
+//   let numberRevArr = numberArr.reverse()
+  console.log(numberArr);
+//   for (let i = 0; i < numberArr.length; i++) {
+    if (numberArr === numberArr.reverse()) 
+      console.log("true");
     
+    
+//   }
 };
+isPalindrome(121);
